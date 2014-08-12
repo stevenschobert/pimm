083b738e96bf7571908d22370813a7aea908c2ac
Steven Schobert <spschobert@gmail.com>
2014-08-12 14:50:10 -0500

    add more tests for Path class

--- test/pimm/support/path_test.js
+++ test/pimm/support/path_test.js
@@ -14,6 +14,32 @@
       p = new Path();
     });
 
+    describe('#extension', function() {
+      it('should return the extension of the file', function() {
+        p.value = './test.js';
+        assert.equal(p.extension(), '.js');
+      });
+    });
+
+    describe('#withoutExtension', function() {
+      it('should return the path of the file without the extension', function() {
+        p.value = './test.js';
+        assert.equal(p.withoutExtension(), './test');
+      });
+    });
+
+    describe('#hasExtension', function() {
+      it('should return true if the extension matches', function() {
+        p.value = './test.js';
+        assert(p.hasExtension('js'));
+      });
+
+      it('should return false if the extension does not match', function() {
+        p.value = './test.js';
+        assert(!p.hasExtension('html'));
+      });
+    });
+
     describe('#withoutPrefix', function() {
       it('should return the path without the prefix', function() {
         p.value = sep+'.'+sep+'1some'+sep+'path';
