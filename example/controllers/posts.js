0adf71d44816604c8639ff534ccf5f495001db7c
Steven Schobert <spschobert@gmail.com>
2014-08-13 17:30:07 -0500

    update example app

--- example/controllers/posts.js
+++ example/controllers/posts.js
@@ -7,12 +7,13 @@
 
   util.inherits(PostsController, Controller);
 
-  PostsController.prototype.create = function create(request) {
-    return this.json({test: true});
-  };
-
   PostsController.prototype.index = function index(request) {
-    return this.json([{all: true}]);
+    var posts = [
+      {title: 'Hello world!'},
+      {title: 'This is my blog'}
+    ];
+
+    return this.render('posts/index', { posts: posts });
   };
 
   module.exports = PostsController;
